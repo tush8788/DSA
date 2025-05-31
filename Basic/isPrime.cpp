5655ad60ad0adeb547e90ec465e3da11c8594a83
Tushar <tusharj8788@gmail.com>
2025-05-31 20:11:14 +0530

    add optimize logic for prime number

--- Basic/isPrime.cpp
+++ Basic/isPrime.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 using namespace std;
 
+//TC -> 0(sqrt(n)) SC -> 0(1)
+bool isPrimeOptimize(int n){
+    if(n<=1) return false;
+    int limit = sqrt(n);
+    for(int i=2;i<=limit;i++){
+        if(n%i==0) return false;
+    }
+    return true;
+}
+
+//time complexity 0(n)
 bool isPrime(int num){
     for(int i=2;i<num;i++){
         if(num % i==0)
@@ -10,6 +21,7 @@ bool isPrime(int num){
 }
 
 int main(){
-    // int num =1;
-    cout<<(isPrime(7)==1 ? "true" : "false")<<endl;
+    // bool isPrimeOrNot = isPrime(7);
+    bool isPrimeOrNot = isPrimeOptimize(6); 
+    cout<<isPrimeOrNot;
 }
