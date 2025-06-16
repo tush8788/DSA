55afbcfd17f431148dbebdf265c18d7f4f597913
Tushar <tusharj8788@gmail.com>
2025-06-16 22:40:15 +0530

    add row and colwise sum

--- 2-D-Array/MatrixSum.cpp
+++ 2-D-Array/MatrixSum.cpp
@@ -5,6 +5,7 @@ using namespace std;
 void sumOfMatrix(vector<vector<int>> arr)
 {
     int rowSize = arr.size();
+    int colSize = arr[0].size();
     int leftSum = 0, rightSum = 0;
     // diagnal left sum TC-O(n) SC-O(1)
     for (int i = 0; i < rowSize; i++)
@@ -20,6 +21,24 @@ void sumOfMatrix(vector<vector<int>> arr)
     }
     cout << "Right Sum :: " << rightSum << endl;
 
+    //row wize
+    for(int i=0;i<rowSize;i++){
+        int rowSum = 0;
+        for(int j=0;j<colSize;j++){
+            rowSum+=arr[i][j];
+        }
+        cout<<"row sum "<<i<<" : "<<rowSum<<endl;
+    }
+
+    // column wise
+    for(int row=0;row<rowSize;row++){
+        int colSum=0;
+        for(int col=0;col<colSize;col++){
+            colSum+=arr[col][row];
+        }
+        cout<<"col sum "<<colSum<<endl;
+    }
+    
 }
 
 int main()
