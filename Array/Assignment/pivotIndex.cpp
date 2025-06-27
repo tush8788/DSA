64ab45bb13af5c77c8c09bb536e28a4c3f94ca4c
Tushar <tusharj8788@gmail.com>
2025-06-27 09:04:58 +0530

    add pivotIndex optimze solution

--- Array/Assignment/pivotIndex.cpp
+++ Array/Assignment/pivotIndex.cpp
@@ -9,7 +9,7 @@ int sum(vector<int>arr,int start,int end){
     }
     return sum;
 }
-
+//brouteForce apporch
 int pivotIndex(vector<int>arr){
     int size = arr.size();
     for(int i=0;i<size;i++){
@@ -20,8 +20,39 @@ int pivotIndex(vector<int>arr){
     return -1;
 }
 
+void printArr(vector<int>arr){
+    for(int i=0;i<arr.size();i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+//optimizeApproch
+//TC O(N) SC - O(N)
+int pivotIndexOptimze(vector<int>arr){
+    int size = arr.size();
+    vector<int> leftSumArr(size,0);
+    vector<int> rightSumArr(size,0);
+    //left sum
+    for(int i=1;i<size;i++){
+        leftSumArr[i] = leftSumArr[i-1]+arr[i-1];
+    }
+
+    //right Sum
+    for(int i=size-2;i>=0;i--){
+        rightSumArr[i]=rightSumArr[i+1]+arr[i+1];
+    }
+
+    for(int i=0;i<size;i++){
+        if(leftSumArr[i]==rightSumArr[i]) return i;
+    }
+    return -1;
+}
+
 int main(){
-    vector<int>arr={1,7,3,6,5,6};
-    cout<<"Pivot index is : "<<pivotIndex(arr)<<endl;
+    vector<int>arr={1,7,3,6,5,9};
+    // cout<<"Pivot index is : "<<pivotIndex(arr)<<endl;
+    cout<<"Pivot index is : "<<pivotIndexOptimze(arr)<<endl;
+    
     return 0;
 }
