7e9612504c730ca1f0a95feabddfdd58b6805bb9
Tushar <tusharj8788@gmail.com>
2025-07-08 08:58:00 +0530

    add new method in find duplicate

--- Array/Assignment/findDiplicateNumFromArr.cpp
+++ Array/Assignment/findDiplicateNumFromArr.cpp
@@ -12,6 +12,7 @@ int findDulicateBrouteForce(vector<int>arr){
 }
 
 //negative marking (visited node mark as negative)
+//TC O(n) sc o(1)
 int findDuplicateFromNegativeMarking(vector<int>arr){
     for(int i=0;i<(int)arr.size();i++){
         int indexVal = arr[i];
@@ -25,10 +26,39 @@ int findDuplicateFromNegativeMarking(vector<int>arr){
     }
     return -1;
 }
+//better code
+int findDuplicateFromNegativeMarkingBetterCode(vector<int>arr){
+    int ans = -1;
+    for(int i=0;i<(int)arr.size();i++){
+        //this will give positive num abs is use for that perpose
+        int indexVal = abs(arr[i]);
 
+        //check current postion we already visited then retun
+        if(arr[indexVal]<0) {
+            ans = indexVal;
+            break;
+        }
+
+        //if not just mark visited 
+        arr[indexVal] *= -1;
+    }
+    return ans;
+}
+
+//using posiotioning
+//tc o(n) sc o(1)
+int findDupUsingPositioning(vector<int>arr){
+    while(arr[0]!=arr[arr[0]]){
+        swap(arr[0],arr[arr[0]]);
+    }
+    return arr[0];
+}
 int main(){
+    //constant [1,n] <--- means value trit as index
     vector<int>arr={1,3,4,2,2};
     // cout<<"duplicate num "<<findDulicateBrouteForce(arr)<<endl;
      cout<<"duplicate num "<<findDuplicateFromNegativeMarking(arr)<<endl;
+     cout<<"duplicate num "<<findDuplicateFromNegativeMarkingBetterCode(arr)<<endl;
+     cout<<"duplicate num  using posiotioning "<<findDupUsingPositioning(arr)<<endl;
     return 0;
 }
