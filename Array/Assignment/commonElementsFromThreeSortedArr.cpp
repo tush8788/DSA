782b297c2a9162cbf1f43eb0351f5e2b24375082
Tushar <tusharj8788@gmail.com>
2025-07-15 08:56:45 +0530

    add another func to find common elem

--- Array/Assignment/commonElementsFromThreeSortedArr.cpp
+++ Array/Assignment/commonElementsFromThreeSortedArr.cpp
@@ -9,6 +9,7 @@ void printArr(vector<int>arr){
     cout<<endl;
 }
 
+//1. tc o(n1+n2+n3) sc o(n)
 vector<int> commonElements(vector<int>arr1,vector<int>arr2,vector<int>arr3){
     vector<int>ans;
     int i=0,j=0,k=0;
@@ -38,11 +39,43 @@ vector<int> commonElements(vector<int>arr1,vector<int>arr2,vector<int>arr3){
     return result;
 }
 
+// tc o(n1+n2+n3) sc o(n)
+vector<int> findCommonSecond(vector<int>arr1,vector<int>arr2,vector<int>arr3){
+    vector<int>ans;
+    set<int>st;
+    int i,j,k,n1=arr1.size(),n2=arr2.size(),n3=arr3.size();
+    i=j=k=0;
+
+    while(i<n1 && j<n2 && k<n3){
+        if(arr1[i]==arr2[j] && arr2[j]==arr3[k]){
+            st.insert(arr1[i]);
+            i++;
+            j++;
+            k++;
+        }else if(arr1[i]<arr2[j]){ //jsut check arr1 value smaller then arr2 value 
+            i++;
+        }else if(arr2[j]<arr3[k]){ //jsut check arr2 value smaller then arr3 value 
+            j++;
+        }else{
+            k++;
+        }
+    }
+
+    //traverse set using forEach
+    for(auto i:st){
+        ans.push_back(i);
+    }
+
+    return ans;
+}
+
 int main(){
     vector<int>a={1,5,10,20,40,80};
     vector<int>b={6,7,20,80,100};
     vector<int>c={3,4,15,20,30,70,80,100};
-    vector<int>ans = commonElements(a,b,c);
+    // vector<int>ans = commonElements(a,b,c);findCommonSecond
+    vector<int>ans = findCommonSecond(a,b,c);
+
     printArr(ans);
     return 0;
 }
