f7a2f71657f4c78ea34121bfefc4e631bea524db
Tushar <tusharj8788@gmail.com>
2025-07-24 09:07:03 +0530

    add sumOfTwoArray func

---
+++ Array/Assignment/factorialOfLargeNum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+vector<int>brouteForceApproch(int num){
+    vector<int>ans;
+    for(int i=1;i<=num;i++){
+        
+    }
+}
+
+int main(){
+    return 0;
+}
---
+++ Array/Assignment/sumOfTwoArray.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+
+void printArr(vector<int>arr){
+    for(int i=0;i<(int)arr.size();i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+// tc o(max(m,n)) sc o(max(m,n))
+vector<int>brouteForce(vector<int>arr1,vector<int>arr2){
+    vector<int>ans;
+    int arr1Size = arr1.size();
+    int arr2Size = arr2.size();
+    int i = arr1Size-1;
+    int j = arr2Size-1;
+    int carry = 0;
+    while(i>=0 || j>=0){
+        if(i>=0 && j>=0){
+            int temp = arr1[i] + arr2[j] + carry;
+            //get last value
+            int lastElem = temp%10;
+            ans.push_back(lastElem);
+            carry = temp/10;
+            i--;
+            j--;
+        }else if(i>=0){
+            int temp = arr1[i] + carry;
+            int lastElem = temp%10;
+            ans.push_back(lastElem);
+            carry = temp/10;
+            i--;
+        }else{
+            int temp = arr1[j] + carry;
+            int lastElem = temp%10;
+            ans.push_back(lastElem);
+            carry = temp/10;
+            j--;
+        }
+    }
+    //if last elem have carry value so just add at last
+    if(carry>0){
+        ans.push_back(carry);
+    }
+
+    //reverse arr
+    i=0,j = ans.size()-1;
+    while(i<j){
+        swap(ans[i],ans[j]);
+        i++;
+        j--;
+    }
+    return ans;
+}
+
+int main(){
+    vector<int>arr1 = {9,5,4,9};
+    vector<int>arr2 = {6,1,4};
+    vector<int> ans = brouteForce(arr1,arr2);
+    printArr(ans);
+    return 0;
+}
