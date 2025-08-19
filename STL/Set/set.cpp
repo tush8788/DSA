396ec75d49967a5202b222d6749ee528f5f63bec
Tushar <tusharj8788@gmail.com>
2025-08-19 08:54:17 +0530

    complete ser

--- STL/Set/set.cpp
+++ STL/Set/set.cpp
@@ -16,6 +16,40 @@ int main(){
         it++;
     }
     cout<<endl;
+
+    //order set
+    set<int>orset;
+    orset.insert(12);
+    orset.insert(1);
+    orset.insert(22);
+    orset.insert(13);
+
+    set<int>::iterator orit = orset.begin();
+    while (orit!=orset.end())
+    {
+        cout<<*orit<<" ";
+        orit++;
+    }
+    cout<<endl;
+
+    //size
+    cout<<"size:: "<<orset.size()<<endl;
+
+    //clear
+    // orset.clear();
+
+    cout<<"after clear size:: "<<orset.size()<<endl;
+
+    cout<<"isEmpty :: "<<(orset.empty()?"true":"false")<<endl;
+    
+    // erase
+    // orset.erase(orset.begin(),orset.end());
+    // cout<<"isEmpty after erase :: "<<(orset.empty()?"true":"false")<<endl;
+
+    //find
+    cout<<"find :: "<<(orset.find(22)!=orset.end()?"found":"not found")<<endl;
+    //count
+    cout<<"count :: "<<(orset.count(12)?"key found ":"not found ");
     
     return 0;
 }
